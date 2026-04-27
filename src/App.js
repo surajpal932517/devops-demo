fa4408cb12ff7c7ad61d5a8316dc25e65caa7102
suraj <surajpal@alohatechnologydev.com>
2026-04-27 18:08:16 +0530

    Added chnages to test 3

--- src/App.js
+++ src/App.js
@@ -20,6 +20,9 @@ function App() {
 	     <h1>Hello DevOps</h1>
        <h1>Hello wwwwww</h1>
        <h1>My DevOps App 🚀</h1>
+       <h1>Hello DevOps</h1>
+       <h1>Hello wwwwww</h1>
+       <h1>My DevOps App 🚀</h1>
       </header>
     </div>
   );
